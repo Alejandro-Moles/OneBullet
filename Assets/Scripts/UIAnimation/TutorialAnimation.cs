cc82365bf9ee1ec49c7039aaf846c57a597f5ff1
Alejandro-Moles <amoleshurtado@gmail.com>
2023-02-15 10:23:13 +0100

    Versión 13
    En esta version he añadido un nuevo tipo de animacion para el texto

--- Assets/Scripts/UIAnimation/TutorialAnimation.cs
+++ Assets/Scripts/UIAnimation/TutorialAnimation.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TutorialAnimation : MonoBehaviour
 {
     private Animator UIAnimator;
     [SerializeField] private TextMeshProUGUI Tutorial_Text;
+    [SerializeField] private TextMeshProUGUI TextBox;
 
     private void Start()
     {
@@ -38,8 +40,26 @@ public class TutorialAnimation : MonoBehaviour
         UIAnimator.SetTrigger("Weapon");
     }
 
+    public void ShowEnemies()
+    {
+        UIAnimator.SetTrigger("Enemies");
+    }
+
+
+
     public void ChangeText(string text)
     {
         Tutorial_Text.text = text;
+    }
+
+
+    public void ChangeTextBox(string text)
+    {
+        TextBox.text = TextBox.text + text;
+    }
+
+    public void WriteSpace()
+    {
+        TextBox.text = TextBox.text + " ";
     }
 }
